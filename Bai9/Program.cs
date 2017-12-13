5332753b9ef8cc136b6e03ba07533d247b4f52eb
nguyenle510 <trthng96@gmail.com>
2017-12-13 21:23:43 +0700

    Add project files.

---
+++ Bai9/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeTrinhHoangAnhTien_buoi9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            string log = "";
+
+            client.Connect("mail.emailserver.vn", 25);
+
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+
+            log += "Server: " + sr.ReadLine() + "\n";
+
+            string data = "HELO";
+
+            log += "Client: " + data + "\n";
+            sw.WriteLine(data);
+            sw.Flush();
+
+            log += "Server: " + sr.ReadLine() + "\n";
+            data = "MAIL FROM: <" + "nguyxxdoxxphoxx.it@gmail.com" + ">";
+            log += "Client: " + data + "\n";
+            sw.WriteLine(data);
+            sw.Flush();
+            log += "Server: " + sr.ReadLine() + "\n";
+            data = "RCPT TO: <" + "phong.nd@emailserver.vn" + ">";
+            log += "Client: " + data + "\n";
+            sw.WriteLine(data);
+            sw.Flush();
+            log += "Server: " + sr.ReadLine() + "\n";
+            data = "DATA";
+            log += "Client: " + data + "\n";
+            sw.WriteLine(data);
+            sw.Flush();
+            log += "Server: " + sr.ReadLine() + "\n";
+            data = emailContent + "\r\n" + ".";
+            log += "Client: " + data + "\n";
+            sw.WriteLine(data);
+            sw.Flush();
+            log += "Server: " + sr.ReadLine() + "\n";
+            data = "QUIT";
+            log += "Client: " + data + "\n";
+            sw.WriteLine(data);
+            sw.Flush();
+            log += "Server: " + sr.ReadLine() + "\n";     
+            sr.Close();
+            sw.Close();
+            client.Close();
+        }
+        public static string emailContent { get; set; }
+    }
+}
